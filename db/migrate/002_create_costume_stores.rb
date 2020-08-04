d1e75a8ed05bcdb8b30a72a33685555611ee1ec7
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-04 17:00:28 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name 
       t.string :location
       t.integer :costume_inventory
-      t.integer :employees
-      t.boolean :open
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.time :opening_time
       t.time :closing_time
     end
